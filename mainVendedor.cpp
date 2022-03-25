75c3ff214b12c4277aee1b18fa70303194c76c85
pedrohfmacedo <101015333+pedrohfmacedo@users.noreply.github.com>
2022-03-24 22:42:22 -0300

    Add files via upload

---
+++ mainVendedor.cpp
@@ -0,0 +1,31 @@
+/*UFCG - CCEI - ENGENHARIA ELETRICA 2020.2
+TECNICAS DE PROGRAMACAO 
+PROFESSOR: MARCUS SALERNO
+GRUPO: David Emanuel Albuquerque dos Santos- Turma 02
+José Vanilson de Brito Júnior - Turma 01
+Lameque Dias de Oliveira - Turma 01
+Pedro Henrique Fernandes Macêdo -  Turma 02
+Felipe Faustino do Nascimento- Turma 02
+Monitora:Ravânia Luciano Martildes
+PROJETO: LOJA DE INSTRUMENTOS MUSIDOM 
+DESCRIÇÃO: O programa deve realizar diversas funções entre eles, o cadastro de 
+produto, de cliente, e vendedores, gerenciar o estoque da loja, realizar pedidos
+MAIN TESTE PARA CLASSE VENDEDOR
+ */
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <iomanip>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+#include "Vendedor.cpp"
+
+int main(){
+
+	Cadastro_vendedor MUSIDOM("Marcus","704.345.184-85",3);
+	MUSIDOM.get_imprimavendedor();
+	
+}
